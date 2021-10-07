2d29bbb6beb2ba643329d906d1ac6c5f81f1d0f5
magdalenamarzec <Marzecmagdalena98@gmail.com>
2021-10-07 13:11:13 +0200

    Okno zdjecia ma lepsze wymiary, choc nadal jest nieco znieksztalcone

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ end_loc = input('Podaj lokalizację, do której mają trafić zdjęcia: ')
 
 for file in os.listdir(str(first_loc)):
     if file.endswith('.jpg'):
+        cv2.namedWindow('bug', cv2.WINDOW_NORMAL)
+        cv2.resizeWindow("bug", 1000, 900)
         cv_photo = cv2.imread('D:/PW/Python/sciezki_folderow/bezmiechowa/'+f'{file}')
         cv2.imshow('bug', cv_photo)
         cv2.waitKey(0)
