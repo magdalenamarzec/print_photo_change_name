fea46d0b0b0c78fa6530839b97164c7b06da35d9
magdalenamarzec <Marzecmagdalena98@gmail.com>
2021-10-07 12:46:43 +0200

    Otwieranie zdjęcia, zmiana nazwy, przeniesienie do nowego folderu2

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+import shutil
+import cv2
+import time
+
+first_loc = input('Podaj lokalizację, w której znajdują się zdjęcia: ')
+end_loc = input('Podaj lokalizację, do której mają trafić zdjęcia: ')
+
+for file in os.listdir(str(first_loc)):
+    if file.endswith('.jpg'):
+        cv_photo = cv2.imread('D:/PW/Python/sciezki_folderow/bezmiechowa/'+f'{file}')
+        cv2.imshow('bug', cv_photo)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        time.sleep(1)
+        shutil.move(first_loc+'/'+f'{file}', end_loc+'/'+f'2021_{file}')
